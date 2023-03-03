0ca012dfe507ad40c09ad52ade6a4177f1130f2f
Gustavo <gust4v0309@gmail.com>
2023-03-03 16:33:11 -0300

    Feat: Learning union types

---
+++ src/A0011 - Union Types/union-types.ts
@@ -0,0 +1,11 @@
+function addOrConcat(a: number | string, b: number | string): number | string {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  } else if (typeof a === 'string' && typeof b === 'string') {
+    return a + b;
+  } else {
+    return 'Inválido';
+  }
+}
+
+console.log(addOrConcat('10', '20'));
